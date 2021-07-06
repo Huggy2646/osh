92c6bac04c7381821a7022476c19d4792c823808
osh990129 <81164267+osh990129@users.noreply.github.com>
2021-07-06 12:25:39 +0900

    Add files via upload

---
+++ six_8.py
@@ -0,0 +1,2 @@
+kor,eng,ma,sc = map(int,input().split())
+print((kor+eng+ma+sc)//4)
---
+++ three_7.py
@@ -0,0 +1,9 @@
+'''
+다음 소스 코드를 완성하여 'Hello, world!'와 'Python Programming'이
+각 줄에 출력되게 만드세요.
+print('Hello, world!')
+print(                  )
+'''
+
+print("Hello, world!")
+print("Python Programing")
