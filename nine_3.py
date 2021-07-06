19d423df6e77aaba6a99690c3280782ca10cb2a1
osh990129 <81164267+osh990129@users.noreply.github.com>
2021-07-06 14:11:40 +0900

    Add files via upload

---
+++ nine_3.py
@@ -0,0 +1,21 @@
+'''
+다음 소스 코드를 완성하여 실행 결과대로 문자열이 출력되게 만드세요.
+
+practice_multiline_string.py
+s =                                                       
+                                                            
+                                                            
+ 
+print(s)
+
+(실행 결과)
+Python is a programming language that lets you work quickly
+and
+integrate systems more effectively.
+'''
+
+s = '''Python is a programming language that lets you work quickly
+and
+integrate systems more effectively.'''
+
+print(s)
